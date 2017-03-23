81d8d23cc3685e9c7ae3819e80124d693e5fabe4
GWh <gunnarwh@stud.ntnu.no>
2017-03-23 20:47:59 +0100

    deformasjo.py: Beregner forskyvninger. Må kalles på riktig så den kjøres...
    k1.py: Validert. Formlene stemmer med dokumentasjonen.
    
    input.ini: Bare endret for å sammenlikne svar med KL_fund

---
+++ deformasjon.py
@@ -0,0 +1,50 @@
+import numpy
+
+
+def _beregn_deformasjon_M(mast, M, x, fh):
+    """Beregner deformasjon D_z i kontakttrådhøyde som følge av moment
+    om  y-aksen med angrepspunkt i høyde x. Dersom FH > x interpoleres
+    forskyvningen til høyde FH ved hjelp av tangens til vinkelen theta
+    i høyde x ganget med høydedifferansen fh - x.
+    """
+    E = mast.E              # [N / mm^2] E-modul, stål
+    Iy = mast.Iy(mast.h)    # [mm^4]
+    M = M * 1000            # Konverterer til [Nmm]
+    x = x * 1000            # Konverterer til [mm]
+    fh = fh * 1000          # Konverterer til [mm]
+
+    if fh > x:
+        theta = (M * x) / (E * Iy)
+        D_z = (M * x ** 2) / (2 * E * Iy) + numpy.tan(theta) * (fh - x)
+    else:
+        D_z = (M * fh ** 2) / (2 * E * Iy)
+    return D_z
+
+
+def _beregn_deformasjon_P(mast, P, x, fh):
+    """Beregner derformasjonen D_z i kontakttrådhøyde pga. punktlast P
+    i angrepspunkt x."""
+
+    E = mast.E                    # [N / mm^2] E-modul, stål
+    Iy = mast.Iy(mast.h * (2/3))  # [mm^4]
+    x = x * 1000                  # [mm]
+    fh = fh * 1000                # [mm]
+
+    delta = (P / (2 * E * Iy)) * (x * fh ** 2 - ((1 / 3) * fh ** 3))
+
+    return delta
+
+
+def _beregn_deformasjon_q(mast, q, x, fh):
+    """Beregner deformasjonen D_z i kontakttråhøyde pga. vindlast, q,
+    i angrepspunkt x."""
+
+    E = mast.E                      # [N / mm^2] E-modul, stål
+    Iy = mast.Iy(mast.h * (2 / 3))  # [mm^4] i mastens 3.delspunkt
+    x = x * 1000                    # [mm]
+    fh = fh * 1000                  # [mm]
+
+    delta = ((q * fh ** 2) / (24 * E * Iy)) * (6 * x ** 2 - 4 * x * fh + fh ** 2)
+
+    return delta
+
--- k1.py
+++ k1.py
@@ -17,16 +17,19 @@ def sidekraft(sys, i, mast, a_T, a_T_dot, a, B1, B2):
     temperatureffekter."""
 
     # Inngangsparametre
-    S = 1000 * sys.kontakttraad["Strekk i ledning"]  # [N]
-    b_mast = mast.bredde(i.h - i.fh) / 1000          # [m]
-    r = i.radius                                     # [m]
-    FH = i.fh                                        # [m]
-    E = mast.E                                       # [N/(mm^2)]
-    alpha = 1.7 * 10 ** (-5)                         # [1/(grader C)]
-    delta_t = 45                                     # [(grader C)]
+    s_kl = 1000 * sys.kontakttraad["Strekk i ledning"]  # [N]
+    s_b = 1000 * sys.baereline["Strekk i ledning"]      # [N]
+    b_mast = mast.bredde(i.h - i.fh) / 1000             # [m]
+    r = i.radius                                        # [m]
+    FH = i.fh                                           # [m]
+    SH = i.sh                                           # [m]
+    E = mast.E                                          # [N/(mm^2)]
+    alpha = 1.7 * 10 ** (-5)                            # [1/(grader C)]
+    delta_t = 45                                        # [(grader C)]
 
     # Initierer Vz, Vy, Mz, My, T = 0 av hensyn til Python
-    V_z = 0
+    V_kl = 0
+    V_b = 0
     V_y = 0
     M_z = 0
     M_y = 0
@@ -35,34 +38,46 @@ def sidekraft(sys, i, mast, a_T, a_T_dot, a, B1, B2):
     if not i.fixpunktmast and not i.fixavspenningsmast:
         if i.strekkutligger:
             # Strekkraft virker i positiv z-retning [N]
-            V_z += S * ((a / r) + 2 * ((B2 - B1) / a))
+            V_kl += s_kl * ((a / r) + 2 * ((B2 - B1) / a))
+            V_b += s_b * (a / r)
 
             # Tilleggskraft dersom siste seksjonsmast før avspenning.
             # Neste mast på den andre siden av sporet.
             if i.siste_for_avspenning and i.master_bytter_side:
-                V_z += S * (a_T / a)
+                V_kl += s_kl * (a_T / a)
+                V_b += s_b * (a_T / a)
                 # Neste mast på samme side av sporet.
             elif i.siste_for_avspenning and not i.master_bytter_side:
-                V_z -= S * (a_T / a)
+                V_kl -= s_kl * (a_T / a)
+                V_b -= s_b * (a_T / a)
             # Momentet gir strekk på baksiden av mast ift. spor [Nm]
-            M_y += V_z * FH
+            M_y += V_kl * FH + (V_b * (FH + SH))
         else:
             # Trykkraft virker i negativ z-retning [N]
-            V_z -= - S * ((a / r) + 2 * ((B2 - B1) / a))
+            V_kl -= s_kl * ((a / r) + 2 * ((B2 - B1) / a))
+            if r < 1200:
+                # Trykkutligger i innerkurve
+                V_b -= s_b * (a / r)
+            else:
+                # Trykkutliggere både i inner og ytterkurve. Velger (+)
+                V_b += s_b * (a / r)
 
             # Tilleggskraft dersom siste seksjonsmast før avspenning [N].
             # Neste mast på den andre siden av sporet.
             if i.siste_for_avspenning and i.master_bytter_side:
-                V_z += S * (a_T_dot / a)
+                V_kl += s_kl * (a_T_dot / a)
+                V_b += s_b * (a_T_dot / a)
                 # Neste mast på den samme siden av sporet.
             elif i.siste_for_avspenning and not i.master_bytter_side:
-                V_z -= S * (a_T_dot / a)
+                V_kl -= s_kl * (a_T_dot / a)
+                V_b -= s_b * (a_T_dot / a)
             # Momentet gir strekk på sporsiden av masten [Nm].
-            M_y -= - V_z * FH
+            M_y -= - V_kl * FH - (V_b * (FH + SH))
 
-    # Forskyvning dz [mm] pga. V_z
+    # Forskyvning dz [mm] pga. V_kl og V_b
     Iy_13 = mast.Iy(mast.h*(2/3))  # Iy i tredjedelspunktet
-    dz_kl = (V_z / (2 * E * Iy_13)) * ((2 / 3) * FH ** 3)
+    dz_kl = (V_kl / (2 * E * Iy_13)) * ((2 / 3) * FH ** 3)
+    dz_b = (V_b / (2 * E * Iy_13)) * ((FH + SH) * FH ** 2 - (1 / 3) * FH ** 3)
 
     # Ved temperaturendring vil KL vandre og utligger følge med.
     # Beregner maksimal skråstilling av utligger [m]
@@ -72,9 +87,9 @@ def sidekraft(sys, i, mast, a_T, a_T_dot, a, B1, B2):
             and not i.linjemast_utliggere == 2:
         # Vandringskraft parallelt spor i primærfelt (én utligger)
         if i.strekkutligger:
-            V_y = V_z * (dl / a_T)      # [N]
+            V_y = V_kl * (dl / a_T)      # [N]
         else:
-            V_y = V_z * (dl / a_T_dot)  # [N]
+            V_y = V_kl * (dl / a_T_dot)  # [N]
         M_z = V_y * FH                  # [Nm]
         T = V_y * b_mast                # [Nm]
 
@@ -86,9 +101,9 @@ def sidekraft(sys, i, mast, a_T, a_T_dot, a, B1, B2):
     R[1][0] = M_y
     R[1][1] = V_y
     R[1][2] = M_z
-    R[1][3] = V_z
+    R[1][3] = V_kl + V_b
     R[1][5] = T
     R[1][7] = dy_vandre
-    R[1][8] = dz_kl
+    R[1][8] = dz_kl + dz_b
 
     return R
