ca200b3973718bf864169341725d5b7845142425
Stanislav Pika <staspika@gmail.com>
2017-09-22 12:16:56 +0200

    Split class definition and object initialization for input data

--- inndata.py
+++ inndata.py
@@ -3,18 +3,81 @@ import configparser
 
 class Inndata(object):
     """Container-klasse for enkel tilgang til inngangsparametre fra .ini-fil."""
-
-    def __init__(self, ini):
+    def __init__(self):
+        # Info
+        self.banestrekning = ""
+        self.km = 0.0
+        self.prosjektnr = 0
+        self.mastenr = 0
+        self.signatur = ""
+        self.dato = ""
+        # Mastealternativer
+        self.siste_for_avspenning = False
+        self.linjemast_utliggere = 0
+        self.avstand_fixpunkt = 0
+        self.fixpunktmast = False
+        self.fixavspenningsmast = False
+        self.avspenningsmast = False
+        self.strekkutligger = False
+        self.master_bytter_side = False
+        self.avspenningsbardun = False
+        # Fastavspente ledninger
+        self.matefjern_ledn = False # Mate- eller fjernledning?
+        self.matefjern_antall = 0 # Antall mate- og fjernledninger
+        self.at_ledn = False # Autotransformatorledning?
+        self.at_type = "" # Type autotransformatorsystem
+        self.forbigang_ledn = False # Forbigangledning?
+        self.jord_ledn = False # Jordledning?
+        self.jord_type = "" # Type jordledning
+        self.fiberoptisk_ledn = False # Fiberoptisk ledning?
+        self.retur_ledn = False # Returledning
+        self.auto_differansestrekk = False # 
+        self.differansestrekk = 0.0
+        # System
+        self.systemnavn = ""
+        self.radius = 0
+        self.a1 = 0.0
+        self.a2 = 0.0
+        self.delta_h1 = 0.0
+        self.delta_h2 = 0.0
+        self.vindkasthastighetstrykk = 0.0
+        # Geometri
+        self.h = 0.0 # Mastehøyde
+        self.hfj = 0.0 # Høyde toppmonterte ledninger
+        self.hf = 0.0 # Høyde forbigangsledning
+        self.hj = 0.0 # Høyde jordledning
+        self.hr = 0.0 # Høyde returledning
+        self.fh = 0.0 # Kontakttrådhøyde
+        self.sh = 0.0 # Utliggerens systemhøyde
+        self.e = 0.0 # Avstand fra skinneoverkant til toppen av mastefundament
+        self.sms = 0.0 # Avstand senter mast – senter spor
+        # Diverse
+        self.s235 = False
+        self.materialkoeff = False
+        self.traverslengde = False
+        self.ec3 = False
+        self.isklasse = ""
+        # Brukerdefinert last
+        self.brukerdefinert_last = False
+        self.f_x = 0.0
+        self.f_y = 0.0
+        self.f_z = 0.0
+        self.e_x = 0.0
+        self.e_y = 0.0
+        self.e_z = 0.0
+        self.a_vind = 0.0
+        self.a_vind_par = 0.0
+        # Hjelpevariabler
+        self.referansevindhastighet = 0
+        self.kastvindhastighet = 0.0
+    def from_ini_file(self, ini):
         """Initialiserer :class:`Inndata`-objekt.
 
         :param ini: .ini-fil for avlesing av inputparametre
         """
-
         cfg = configparser.ConfigParser()
-        cfg.read("input.ini")
-
+        cfg.read(ini)
         # Oppretter variabler for data fra .ini-fil
-
         # Info
         self.banestrekning = cfg.get("Info", "banestrekning")
         self.km = cfg.getfloat("Info", "km")
@@ -22,7 +85,6 @@ class Inndata(object):
         self.mastenr = cfg.getint("Info", "mastenr")
         self.signatur = cfg.get("Info", "signatur")
         self.dato = cfg.get("Info", "dato")
-
         # Mastealternativer
         self.siste_for_avspenning = cfg.getboolean("Mastealternativer", "siste_for_avspenning")
         self.linjemast_utliggere = cfg.getint("Mastealternativer", "linjemast_utliggere")
@@ -33,7 +95,6 @@ class Inndata(object):
         self.strekkutligger = cfg.getboolean("Mastealternativer", "strekkutligger")
         self.master_bytter_side = cfg.getboolean("Mastealternativer", "master_bytter_side")
         self.avspenningsbardun = cfg.getboolean("Mastealternativer", "avspenningsbardun")
-
         # Fastavspente ledninger
         self.matefjern_ledn = cfg.getboolean("Fastavspent", "matefjern_ledn")
         self.matefjern_antall = cfg.getint("Fastavspent", "matefjern_antall")
@@ -46,7 +107,6 @@ class Inndata(object):
         self.retur_ledn = cfg.getboolean("Fastavspent", "retur_ledn")
         self.auto_differansestrekk = cfg.getboolean("Fastavspent", "auto_differansestrekk")
         self.differansestrekk = cfg.getfloat("Fastavspent", "differansestrekk")
-
         # System
         self.systemnavn = cfg.get("System", "systemnavn")
         self.radius = cfg.getint("System", "radius")
@@ -55,7 +115,6 @@ class Inndata(object):
         self.delta_h1 = cfg.getfloat("System", "delta_h1")
         self.delta_h2 = cfg.getfloat("System", "delta_h2")
         self.vindkasthastighetstrykk = cfg.getfloat("System", "vindkasthastighetstrykk")
-
         # Geometri
         self.h = cfg.getfloat("Geometri", "h")
         self.hfj = cfg.getfloat("Geometri", "hfj")
@@ -66,14 +125,12 @@ class Inndata(object):
         self.sh = cfg.getfloat("Geometri", "sh")
         self.e = cfg.getfloat("Geometri", "e")
         self.sms = cfg.getfloat("Geometri", "sms")
-
         # Diverse
         self.s235 = cfg.getboolean("Div", "s235")
         self.materialkoeff = cfg.getfloat("Div", "materialkoeff")
         self.traverslengde = cfg.getfloat("Div", "traverslengde")
         self.ec3 = cfg.getboolean("Div", "ec3")
         self.isklasse = cfg.get("Div", "isklasse")
-
         # Brukerdefinert last
         self.brukerdefinert_last = cfg.getboolean("Brukerdefinert last", "brukerdefinert_last")
         self.f_x = cfg.getfloat("Brukerdefinert last", "f_x")
@@ -84,9 +141,6 @@ class Inndata(object):
         self.e_z = cfg.getfloat("Brukerdefinert last", "e_z")
         self.a_vind = cfg.getfloat("Brukerdefinert last", "a_vind")
         self.a_vind_par = cfg.getfloat("Brukerdefinert last", "a_vind_par")
-
         # Hjelpevariabler
         self.referansevindhastighet = cfg.getint("Hjelpevariabler", "referansevindhastighet")
         self.kastvindhastighet = cfg.getfloat("Hjelpevariabler", "kastvindhastighet")
-
-
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import beregning
 import time
 import inndata
 
-
 def beregn_master(ini):
     """Kjører beregningsprosedyre.
 
@@ -16,52 +15,34 @@ def beregn_master(ini):
      og ``bjelkemaster_sortert``, kopi av input for aktuell beregning ``i``
     :rtype: :class:`list`, :class:`list`, :class:`Inndata`
     """
-
     masteliste = []
-
     # Oppretter inndataobjekt med data fra .ini-fil
     i = inndata.Inndata(ini)
     masteliste.extend(beregning.beregn(i))
-
     for mast in masteliste:
         mast.sorter_grenseverdier()
-
     gittermaster = masteliste[0:7]
     bjelkemaster = masteliste[7:]
     gittermaster_sortert = sorted(gittermaster, key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad, reverse=True)
     bjelkemaster_sortert = sorted(bjelkemaster, key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad, reverse=True)
-
     return gittermaster_sortert, bjelkemaster_sortert, i
-
-
 
 if __name__ == "__main__":
     # Kjører kun ved direkte kjøring av main.py
-
     # Tester kjøretid
     start_time = time.clock()
-
     ###################################################################
-
     print("\nVelkommen til Bane NORs fantastiske nye beregningsverktøy!\n")
-
     values = []
-
-    with open("input.ini", "r") as ini:
-        # Oppretter inndataobjekt med data fra .ini-fil
-        i = inndata.Inndata(ini)
-        masteliste = beregning.beregn(i)
-
+    # Oppretter inndataobjekt med data fra .ini-fil
+    i = inndata.Inndata()
+    i.from_ini_file("input.ini")
+    masteliste = beregning.beregn(i)
     for mast in masteliste:
         mast.sorter_grenseverdier()
     master_sortert = sorted(masteliste, key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad, reverse=True)
     for mast in master_sortert:
         print("Navn: {}     UR = {:.3g} %".format(mast.navn, 100*mast.bruddgrense[0].utnyttelsesgrad))
     print()
-
-
     ###################################################################
-
     print("--- %s seconds ---" % (time.clock() - start_time))
-
-
