e1841f2593440c0eaff335db4a23c8f4908acd54
Stanislav Pika <staspika@gmail.com>
2019-01-31 13:13:42 +0100

    Small syntax cleanup

--- main.py
+++ main.py
@@ -16,52 +16,40 @@ def beregn_master(ini):
      og ``bjelkemaster_sortert``, kopi av input for aktuell beregning ``i``
     :rtype: :class:`list`, :class:`list`, :class:`Inndata`
     """
-
     masteliste = []
-
-    # Oppretter inndataobjekt med data fra .ini-fil
-    i = inndata.Inndata(ini)
+    i = inndata.Inndata(ini)  # Oppretter inndataobjekt fra .ini-fil
     masteliste.extend(beregning.beregn(i))
-
     for mast in masteliste:
         mast.sorter_grenseverdier()
-
     gittermaster = masteliste[0:7]
     bjelkemaster = masteliste[7:]
-    gittermaster_sortert = sorted(gittermaster, key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad, reverse=True)
-    bjelkemaster_sortert = sorted(bjelkemaster, key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad, reverse=True)
-
+    gittermaster_sortert = sorted(
+        gittermaster,
+        key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad,
+        reverse=True)
+    bjelkemaster_sortert = sorted(
+        bjelkemaster,
+        key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad,
+        reverse=True)
     return gittermaster_sortert, bjelkemaster_sortert, i
 
 
-
 if __name__ == "__main__":
-    # Kjører kun ved direkte kjøring av main.py
-
-    # Tester kjøretid
     start_time = time.clock()
-
-    ###################################################################
-
-    print("\nVelkommen til Bane NORs fantastiske nye beregningsverktøy!\n")
-
-    values = []
-
+    print()
+    print("Velkommen til Bane NORs fantastiske nye beregningsverktøy!")
+    print()
     with open("input.ini", "r") as ini:
-        # Oppretter inndataobjekt med data fra .ini-fil
         i = inndata.Inndata(ini)
         masteliste = beregning.beregn(i)
-
     for mast in masteliste:
         mast.sorter_grenseverdier()
-    master_sortert = sorted(masteliste, key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad, reverse=True)
+    master_sortert = sorted(
+        masteliste,
+        key=lambda mast: mast.tilstand_UR_max.utnyttelsesgrad,
+        reverse=True)
     for mast in master_sortert:
-        print("Navn: {}     UR = {:.3g} %".format(mast.navn, 100*mast.bruddgrense[0].utnyttelsesgrad))
-    print()
-
-
-    ###################################################################
-
-    print("--- %s seconds ---" % (time.clock() - start_time))
-
-
+        print("Type {:6} UR = {:>6.2%}".format(
+            mast.navn, mast.bruddgrense[0].utnyttelsesgrad))
+    exec_time = time.clock() - start_time
+    print("Executed in {:.3f} s.".format(exec_time))
